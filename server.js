d04fe2528e50140107aa7a39b14c3afe2f7e4c4e
BhavyaJohar <bhavyarjohar@gmail.com>
2024-10-08 21:23:17 -0400

    Fix port binding for deployment

--- server.js
+++ server.js
@@ -91,6 +91,6 @@ app.get('/api/getLatestOptionsData', (req, res) => {
 
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
